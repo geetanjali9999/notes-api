28cbfba90af0b065b6d97cd2f71d5bb5f2c41255
geetanjali9999 <geetanjalimakhija9999@gmail.com>
2026-05-21 16:39:59 +0530

    add registor/login/logout with authenciation sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // register user
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    // login user
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invaild creadentials'
+            ], 401);
+        }
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('auth_token')->plainTextToken
+        ]);
+    }
+
+
+
+    // logout user
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,32 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NoteController;
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
 });
 
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+// Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('notes', [NoteController::class, 'index']);
+    Route::get('notes/{id?}', [NoteController::class, 'show']);
+    Route::put('notes/{id}', [NoteController::class, 'update']);
+    Route::post('notes', [NoteController::class, 'store']);
+    Route::delete('notes/{id}', [NoteController::class, 'destroy']);
+});
 // Route::apiResource('notes', NoteController::class); //for at one bease url for api
-Route::get('notes', [NoteController::class, 'index']);
-Route::get('notes/{id?}', [NoteController::class, 'show']);
-Route::put('notes/{id}', [NoteController::class, 'update']);
-Route::post('notes', [NoteController::class, 'store']);
-Route::delete('notes/{id}', [NoteController::class, 'destroy']);
+// Route::get('notes', [NoteController::class, 'index']);
+// Route::get('notes/{id?}', [NoteController::class, 'show']);
+// Route::put('notes/{id}', [NoteController::class, 'update']);
+// Route::post('notes', [NoteController::class, 'store']);
+// Route::delete('notes/{id}', [NoteController::class, 'destroy']);
 
