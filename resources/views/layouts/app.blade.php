8f5f5d60c18ab4c5089765d5fcab6afa91a652c2
geetanjali9999 <geetanjalimakhija999@gmail.com>
2026-06-22 17:29:36 +0530

    add layouts/app file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!-- resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Notes App</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    @include('partials.header')
+
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+</body>
+</html>
