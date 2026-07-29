71c81b23c5538c3d34d0645dde158ca1565f27a5
geetanjali9999 <geetanjalimakhija999@gmail.com>
2026-07-29 13:22:33 +0530

    change theme on bootstrap way

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,47 +1,49 @@
 import './bootstrap';
+// -------------------------------------------------- on my way adding theme 
+
+
 // const themeToggle = document.getElementById('themeToggle');
 // const html = document.documentElement;
 
-// // On page load, apply saved preference
-// const savedTheme = localStorage.getItem('theme') || 'light';
-// html.setAttribute('data-theme', savedTheme);
+// // on page load, apply saved prefernce
+// // const savedTheme = localStorage.getItem('theme')  || 'light';
+// const savedTheme = localStorage.getItem('theme') 
+//     || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+// html.setAttribute('data-theme',savedTheme);
 // updateButtonText(savedTheme);
 
-// themeToggle.addEventListener('click', function () {
-//     const currentTheme = html.getAttribute('data-theme');
-//     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+// themeToggle.addEventListener('click' , function() {
+//     const currentTheme =html.getAttribute('data-theme');
+//     const newTheme= currentTheme === 'dark' ? 'light' : 'dark';
 
 //     html.setAttribute('data-theme', newTheme);
 //     localStorage.setItem('theme', newTheme);
 //     updateButtonText(newTheme);
 // });
 
-// function updateButtonText(theme) {
-//     themeToggle.textContent = theme === 'dark' ? '☀️ Light Mode' : '🌙 Dark Mode';
+// function updateButtonText(theme){
+//     themeToggle.textContent =theme ==='dark' ? '☀️ Light Mode' : '🌙 Dark Mode';
 // }
 
-// __________________________________________________
 
+// ---------------------------------------------- bootstrap way toggle the theme dark | light
 
 const themeToggle = document.getElementById('themeToggle');
 const html = document.documentElement;
 
-// on page load, apply saved prefernce
-// const savedTheme = localStorage.getItem('theme')  || 'light';
-const savedTheme = localStorage.getItem('theme') 
-    || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-html.setAttribute('data-theme',savedTheme);
+const savedTheme = localStorage.getItem('theme') || 'light';
+html.setAttribute('data-bs-theme', savedTheme);
 updateButtonText(savedTheme);
 
-themeToggle.addEventListener('click' , function() {
-    const currentTheme =html.getAttribute('data-theme');
-    const newTheme= currentTheme === 'dark' ? 'light' : 'dark';
+themeToggle.addEventListener('click', function () {
+    const currentTheme = html.getAttribute('data-bs-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
 
-    html.setAttribute('data-theme', newTheme);
+    html.setAttribute('data-bs-theme', newTheme);
     localStorage.setItem('theme', newTheme);
     updateButtonText(newTheme);
 });
 
-function updateButtonText(theme){
-    themeToggle.textContent =theme ==='dark' ? '☀️ Light Mode' : '🌙 Dark Mode';
+function updateButtonText(theme) {
+    themeToggle.textContent = theme === 'dark' ? '☀️ Light Mode' : '🌙 Dark Mode';
 }
